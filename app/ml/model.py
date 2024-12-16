087e4fb100bf54c877465b512ce40fe28a58dcc4
pyann-gitpro <paaeho.yann.pro@gmail.com>
2024-12-17 00:13:33 +0100

    problem modeljose réglé, new problem numpy_core

--- app/ml/model.py
+++ app/ml/model.py
@@ -1,5 +1,13 @@
 import pickle
+import os
 
-with open('modeljose.pkl', 'rb') as file:
+# Récupère le chemin du dossier où se trouve le fichier actuel
+base_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(base_dir, 'modeljose.pkl')
+
+# Chargez le fichier
+with open(model_path, 'rb') as file:
+    # Votre code pour lire le modèle
+
     model = pickle.load(file)
 print(model)
